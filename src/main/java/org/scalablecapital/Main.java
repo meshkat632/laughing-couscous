4a5cd97fdc110388a59a013253ce72e85600552e
meshkat <meshkat632@gmail.com>
2018-06-28 01:44:53 +0200

    completed

--- src/main/java/org/scalablecapital/Main.java
+++ src/main/java/org/scalablecapital/Main.java
@@ -1,6 +1,8 @@
-package com.sixt.platform.interview;
+package org.scalablecapital;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -9,28 +11,28 @@ import java.util.stream.Collectors;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
-public class CompletableFutureSyncTest {
+public class Main {
 
+    public static final int EXECUTORS_COUNT = 100;
+    public static final int NUMBER_OF_SEARCH_PAGES = 200;
+    public static final int TIMEOUT = 50000;
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Executor executor = Executors.newFixedThreadPool(1);
-        Set<String> links = GoogleSearcher.getDataFromGoogle("reactjs example");
-        List<CompletableFuture<Set<String>>> pageContentFutures = links.stream().map(link -> {
-            System.out.println("creating task for link:" + link);
+
+        if(args.length < 1) throw new IllegalArgumentException("the search term must be passed as first argument!");
+        final String searchKey = args[0];
+        Executor executor = Executors.newFixedThreadPool(EXECUTORS_COUNT);
+        CompletableFuture<Set<String>> seatchResultFuture = GoogleSearchHelper.searchGoogle(searchKey, NUMBER_OF_SEARCH_PAGES, TIMEOUT);
+        Set<String> pageLinks = seatchResultFuture.get();
+
+        List<CompletableFuture<Set<String>>> pageContentFutures = pageLinks.stream().map(link -> {
+            //System.out.println("creating task for link:" + link);
 
             CompletableFuture<Set<String>> future = CompletableFuture.supplyAsync(new Supplier<Set<String>>() {
                 @Override
                 public Set<String> get() {
-
-                    Set<String> scripts = PageInspector.getJavascripts("https://"+link);
-
-
-                    for(String script : scripts){
-                        System.out.println("---"+script);
-                    }
-
+                    Set<String> scripts = PageInspector.getJavascripts(link, TIMEOUT);
                     return scripts;
-
                 }
             }, executor);
             return future;
@@ -40,19 +42,24 @@ public class CompletableFutureSyncTest {
                 pageContentFutures.toArray(new CompletableFuture[pageContentFutures.size()])
         );
 
-
         CompletableFuture<List<Set<String>>> allPageContentsFuture = allFutures.thenApply(v -> {
             return pageContentFutures.stream()
                     .map(pageContentFuture -> pageContentFuture.join())
                     .collect(Collectors.toList());
         });
+
         CompletableFuture<Map<String, Long>> countFuture = allPageContentsFuture.thenApply(pageContents -> {
             return pageContents.stream().flatMap(Set::stream).collect(groupingBy(Function.identity(), counting()));
         });
 
-        System.out.println(" result:" +  countFuture.get());
+        countFuture.get().entrySet().stream()
+                .sorted((t1, t2) -> Long.compare(t2.getValue(), t1.getValue()))
+                .limit(5)
+                .forEach(item ->{
+                    System.out.println(item );
+                });
+        ((ExecutorService) executor).shutdown();
 
-       // allPageContentsFuture.get();
     }
 }
 
